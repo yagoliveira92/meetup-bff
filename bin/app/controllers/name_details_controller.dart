2f99ee1ba9323a8e19a3f7ef28b2db4786bce49f
Yago Oliveira <yagoliveira92@gmail.com>
2022-12-21 23:48:47 -0300

    Remove the content of controller

--- bin/app/controllers/name_details_controller.dart
+++ bin/app/controllers/name_details_controller.dart
@@ -3,11 +3,6 @@ import 'dart:convert';
 import 'package:shelf/shelf.dart';
 import 'package:http/http.dart' as http;
 
-import '../models/activity_model.dart';
-import '../models/age_model.dart';
-import '../models/gender_model.dart';
-import '../models/nation_model.dart';
-
 class NameDetailsController {
   static nameDetailsController(Request request) async {
     Map<String, dynamic> requestBody =
@@ -24,48 +19,9 @@ class NameDetailsController {
 
     final urlActivy = Uri.https('boredapi.com', '/api/activity');
 
-    final resultAge = await http.get(urlAge);
-    if (resultAge.statusCode > 300) {
-      return Response(resultAge.statusCode, body: resultAge.body);
-    }
-
-    final resultGender = await http.get(urlGender);
-    if (resultGender.statusCode > 300) {
-      return Response(resultGender.statusCode, body: resultGender.body);
-    }
-
-    final resultNationalize = await http.get(urlNationalize);
-    if (resultNationalize.statusCode > 300) {
-      return Response(resultNationalize.statusCode,
-          body: resultNationalize.body);
-    }
-
-    final resultActivity = await http.get(urlActivy);
-    if (resultActivity.statusCode > 300) {
-      return Response(resultActivity.statusCode, body: resultActivity.body);
-    }
-
-    final ageDecode = json.decode(resultAge.body);
-    final genderDecode = json.decode(resultGender.body);
-    final nationalizeDecode = json.decode(resultNationalize.body);
-    final activityDecode = json.decode(resultActivity.body);
-
-    final ageModel = AgeModel.fromJson(ageDecode);
-    final genderModel = GenderModel.fromJson(genderDecode);
-    final nationModel = NationModel.fromJson(nationalizeDecode);
-    final activityModel = ActivityModel.fromJson(activityDecode);
-
-    Map<String, dynamic> mapResponse = {
-      'name': ageModel.name,
-      'age': ageModel.age,
-      'gender': genderModel.gender,
-      'nationality': nationModel.country?[0].countryId,
-      'suggestion_activity': activityModel.activity,
-    };
-
     return Response(
       200,
-      body: JsonEncoder.withIndent(' ').convert(mapResponse),
+      body: JsonEncoder.withIndent(' ').convert({}),
     );
   }
 }
