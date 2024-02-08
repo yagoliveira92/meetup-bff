a676bd9dcd2f0e1b1a57cf9a580083ffea92c02c
Yago Souza Oliveira <yagoliveira92@gmail.com>
2024-02-08 16:40:46 -0300

    Adicionando os isolates novamente

--- bin/server.dart
+++ bin/server.dart
@@ -1,13 +1,30 @@
 import 'dart:io';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
+import 'package:args/args.dart' show ArgParser;
 
 import 'app/routes/routes.dart';
 
 void main(List<String> args) async {
   // Use any available host or container IP (usually `0.0.0.0`).
-  final ip = InternetAddress.anyIPv4;
+  var parser = ArgParser()
+    ..addOption('port', abbr: 'p', defaultsTo: '8080')
+    ..addOption('isolates', abbr: 'i', defaultsTo: '3');
+  var arguments = parser.parse(args);
 
+  var nbOfIsolates = int.parse(arguments['port']);
+  for (int i = 1; i < nbOfIsolates; i++) {
+    Isolate.spawn(_startShelfServer, [int.parse(arguments['port'])]);
+  }
+  _startShelfServer([int.parse(arguments['port'])]);
+
+}
+
+_startShelfServer(List args) async {
+  int port = args[0];
+
+
+  var handler = const Pipeline()
   // Configure a pipeline that logs requests.
   final handler = Pipeline()
       .addMiddleware(
@@ -17,8 +34,16 @@ void main(List<String> args) async {
         Routes.routes(),
       );
 
+  final server = await shelf_io.serve(
   // For running in containers, we respect the PORT environment variable.
+    handler,
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
+    InternetAddress.anyIPv4, // Allows external connections
   final server = await serve(handler, ip, port);
+    port,
   print('Server listening on port ${server.port}');
+    shared: true,
+  );
+  print(
+      'Serving at http://${server.address.host}:${server.port} - isolate: ${Isolate.current.hashCode}');
 }
